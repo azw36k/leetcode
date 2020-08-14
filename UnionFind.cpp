c99f4c7d39b278b6da465a7ca23fa74925cc2db0
azw36k <46640705+azw36k@users.noreply.github.com>
2020-08-14 02:37:54 -0700

    Update UnionFind.cpp

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,27 +1,72 @@
-class UF {
+// ----------------Version 2.0-----------------
+class UnionFind {
+private:
     vector<int> p;
     vector<int> r;
-public:
-    UF(int n) : r(n + 1, 0), p(n + 1, 0) {
-        for (int i = 0; i < p.size(); i++)
-            p[i] = i;
+public: 
+    explicit UnionFind(int n) : p(n), r(n) { iota(begin(p), end(p), 0); }; // p[i] = i
+    int find(int x) {
+        return p[x] == x ? x : p[x] = find(p[x]); 
     }
-    bool union(int u, int v) {
+    bool unio(int u, int v) {
         int pu = find(u);
         int pv = find(v);
         if (pu == pv) return false;
-        if (r[pv] < r[pu])
-            p[pv] = pu;
-        else if (r[pu] < r[pv])
+        if (r[pu] == r[pv]) {
             p[pu] = pv;
-        else {
-            p[pv] = pu;
-            r[pu] += 1;
-        }
+            ++r[pu];
+        } else if (r[pu] < r[pv])
+            p[pu] = pv;
+        else p[pv] = pu;
+        return true;
     }
-    int find(int u) {
-        if (u != p[u])
-            p[u] = find(p[u]);
-        return p[u];
-    }
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------Version 1.0-----------------
+// class UF {
+//     vector<int> p;
+//     vector<int> r;
+// public:
+//     UF(int n) : r(n + 1, 0), p(n + 1, 0) {
+//         for (int i = 0; i < p.size(); i++)
+//             p[i] = i;
+//     }
+//     bool union(int u, int v) {
+//         int pu = find(u);
+//         int pv = find(v);
+//         if (pu == pv) return false;
+//         if (r[pv] < r[pu])
+//             p[pv] = pu;
+//         else if (r[pu] < r[pv])
+//             p[pu] = pv;
+//         else {
+//             p[pv] = pu;
+//             r[pu] += 1;
+//         }
+//     }
+//     int find(int u) {
+//         if (u != p[u])
+//             p[u] = find(p[u]);
+//         return p[u];
+//     }
+// }
