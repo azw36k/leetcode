28f4838f2b93f54d2c1748a8a861eb8089c7f781
azw36k <46640705+azw36k@users.noreply.github.com>
2020-08-29 01:49:17 -0700

    Update UnionFind.cpp

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,4 +1,4 @@
-/* DSU */
+// ----------------DSU----------------
 
 class DSU {
     vector<int> p;
@@ -17,6 +17,7 @@ public:
 };
 
 // ----------------Version 2.0-----------------
+
 class UnionFind {
 private:
     vector<int> p;
@@ -39,52 +40,3 @@ public:
         return true;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------Version 1.0-----------------
-// class UF {
-//     vector<int> p;
-//     vector<int> r;
-// public:
-//     UF(int n) : r(n + 1, 0), p(n + 1, 0) {
-//         for (int i = 0; i < p.size(); i++)
-//             p[i] = i;
-//     }
-//     bool union(int u, int v) {
-//         int pu = find(u);
-//         int pv = find(v);
-//         if (pu == pv) return false;
-//         if (r[pv] < r[pu])
-//             p[pv] = pu;
-//         else if (r[pu] < r[pv])
-//             p[pu] = pv;
-//         else {
-//             p[pv] = pu;
-//             r[pu] += 1;
-//         }
-//     }
-//     int find(int u) {
-//         if (u != p[u])
-//             p[u] = find(p[u]);
-//         return p[u];
-//     }
-// }
