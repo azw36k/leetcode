d09eedfa82ad2ec686e04ef3e28a577ef01ba05c
azw36k <46640705+azw36k@users.noreply.github.com>
2020-09-25 19:55:26 -0700

    Update pair_hash.cpp

--- pair_hash.cpp
+++ pair_hash.cpp
@@ -1,5 +1,5 @@
 struct pair_hash {
-	template <type t1, type t2>
+	template <typename t1, typename t2>
 	size_t operator () (pair<t1, t2> const& p) const {
 		size_t h1 = hash<t1>()(p.first);
 		size_t h2 = hash<t2>()(p.second);
